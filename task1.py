1c9a16961cab1552e7cd3b4d6d379944aacd8106
gortibaldik <ferotre@gmail.com>
2022-04-09 18:16:55 +0200

    TASK1: refactor

--- task1.py
+++ task1.py
@@ -6,12 +6,6 @@ from tseitin_encoding.ast_tree import ASTAbstractNode
 from tseitin_encoding.parsing_utils import create_abstract_syntax_tree
 from tseitin_encoding.tseitin_transformation import turn_nnf_to_tseitin
 
-def get_sorting_item(x: str):
-    if "__spec__" in x:
-        val = x.split("__spec__")[1]
-        return (1, int(val))
-    return (0, x)
-
 def transform_to_numbers(tseitin_ast_tree_root: ASTAbstractNode, mapping):
     subformulas = []
     for sf in tseitin_ast_tree_root.children:
@@ -39,7 +33,13 @@ def transform_to_numbers(tseitin_ast_tree_root: ASTAbstractNode, mapping):
             subformulas.append(new_tokens)
     return subformulas
 
-def print_to_dmacs(tseitin_ast_tree_root: ASTAbstractNode, file):
+def get_sorting_item(x: str):
+    if "__spec__" in x:
+        val = x.split("__spec__")[1]
+        return (1, int(val))
+    return (0, x)
+
+def _print_to_dmacs(tseitin_ast_tree_root: ASTAbstractNode, file):
     count_stats = tseitin_ast_tree_root.count()
     variables = sorted(list(count_stats["variables"]), key=get_sorting_item)
     mapping = {}
@@ -55,6 +55,13 @@ def print_to_dmacs(tseitin_ast_tree_root: ASTAbstractNode, file):
     print(f"p cnf {len(count_stats['variables'])} {len(subformulas)}")
     for sf in subformulas:
         print(" ".join([str(tk) for tk in sf]), file=file)
+
+def print_to_dmacs(tseitin_ast_tree_root: ASTAbstractNode, file):
+    if file is not None:
+        with open(file, 'w') as f:
+            _print_to_dmacs(tseitin_ast_tree_root, f)
+    else:
+        _print_to_dmacs(tseitin_ast_tree_root, None)
 
 def tseitin_encoding(formula: str, nnf_reduce_implications: bool):
     ast_tree_root = create_abstract_syntax_tree(formula)
@@ -79,21 +86,20 @@ def read_formula(input_file):
     logger.debug(f"read formula: {formula}")
     return formula
 
+def set_debug_level(is_debug: bool):
+    if is_debug:
+        logzero.loglevel(logzero.DEBUG)
+    else:
+        logzero.loglevel(logzero.INFO)
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
 
-    if args.debug:
-        logzero.loglevel(logzero.DEBUG)
-    else:
-        logzero.loglevel(logzero.INFO)
+    set_debug_level(args.debug)
     formula = read_formula(args.input)
     tseitin_ast_tree_root = tseitin_encoding(formula, nnf_reduce_implications=args.only_left_to_right)
-    if args.output:
-        with open(args.output, 'w') as f:
-            print_to_dmacs(tseitin_ast_tree_root, f)
-    else:
-        print_to_dmacs(tseitin_ast_tree_root, None)
+    print_to_dmacs(tseitin_ast_tree_root, args.output)
 
 if __name__ == "__main__":
     main()
