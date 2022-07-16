948194fefef537fad14e95c063d4a03a7d01faf2
gortibaldik <ferotre@gmail.com>
2022-07-16 18:20:30 +0200

    prod using volume for everything instead of binded mounts

--- scripts/start_docker_dev.sh
+++ scripts/start_docker_dev.sh
@@ -1,1 +1,2 @@
-docker compose -f docker-compose.dev.yml --project-name satsolver_dev up -d --force-recreate --build
+docker compose -f docker-compose.dev.yml --project-name satsolver_dev  build --no-cache
+docker compose -f docker-compose.dev.yml --project-name satsolver_dev up -d --force-recreate
