8abd596378a68f0ba8373e0678e7f70eb19cf6ff
gortibaldik <ferotre@gmail.com>
2022-08-08 18:54:24 +0200

    standardize base image build

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -2,4 +2,4 @@
 # remove all compiled python
 find . -name "*.pyc" -delete
 
-docker build --no-cache -t base_image:latest -f dockerfiles/dockerfile.base_image .
+docker build --no-cache -t satsolver_base:latest -f dockerfiles/dockerfile.base_image .
