3f5abe97594b4f65a0468f5a14d5d8d016954f21
gortibaldik <ferotre@gmail.com>
2022-08-10 20:42:58 +0200

    Init iteration solution

---
+++ satsolver/task2_iteration.py
@@ -0,0 +1,50 @@
+from satsolver.dpll.assignment import assign_true, unassign, unassign_multiple
+from satsolver.dpll.decision_variable_selection import dec_var_selection
+from satsolver.dpll.structures_preparation import health_check, prepare_structures
+from satsolver.dpll.unit_propagation import unit_propagation
+import satsolver.utils.general_setup as general_setup
+
+from satsolver.dpll.dpll_cycle import DPLLCycle
+
+def get_info():
+  return general_setup.get_info(
+    name="DPLL.v4.iter",
+    taskName="TASK 2 - iter",
+    benchmarkable=True
+  )
+
+def find_model(
+  *,
+  input_file=None,
+  warning=False,
+  debug=False,
+  output_to_stdout=False,
+  nnf_reduce_implications=True
+):
+  dpll = DPLLCycle(
+    prepare_structures=prepare_structures,
+    unit_propagation=unit_propagation,
+    dec_var_selection=dec_var_selection,
+    assign_true=assign_true,
+    unassign=unassign,
+    unassign_multiple=unassign_multiple
+  )
+  dpll.health_check = health_check
+  return general_setup.find_model(
+    dpll.dpll,
+    input_file=input_file,
+    warning=warning,
+    debug=debug,
+    output_to_stdout=output_to_stdout,
+    nnf_reduce_implications=nnf_reduce_implications
+  )
+
+if __name__ == "__main__":
+  parser = general_setup.create_parser()
+  args = parser.parse_args()
+  find_model(
+      input_file=args.input_file,
+      warning=args.warning,
+      debug=args.debug,
+      output_to_stdout=args.output_to_stdout
+  )
