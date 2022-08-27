2ed7e85e299e1b95042a3458fc35551406896b37
gortibaldik <ferotre@gmail.com>
2022-08-27 02:06:20 +0200

    Add max learned clauses stats to database

--- satsolver/utils/stats.py
+++ satsolver/utils/stats.py
@@ -6,19 +6,22 @@ class SATSolverStats:
   unitProps: int              = 0
   decVars: int                = 0
   conflicts: int              = 0
+  learnedClausesPeak: int     = 0
 
   def update(self, stats):
     self.unitProps              += stats.unitProps
     self.decVars                += stats.decVars
     self.unitPropCheckedClauses += stats.unitPropCheckedClauses
     self.conflicts              += stats.conflicts
+    self.learnedClausesPeak     += stats.learnedClausesPeak
   
   def divide(self, total):
     self.unitProps /= total
     self.unitPropCheckedClauses /= total
     self.decVars /= total
     self.conflicts /= total
+    self.learnedClausesPeak /= total
   
   def __repr__(self):
     return f"decs: {self.decVars}; unit: {self.unitProps}; unit_checked: " +\
-           f"{self.unitPropCheckedClauses}; conflicts: {self.conflicts}"
+           f"{self.unitPropCheckedClauses}; conflicts: {self.conflicts}; learnedClausesPeak: {self.learnedClausesPeak}"
